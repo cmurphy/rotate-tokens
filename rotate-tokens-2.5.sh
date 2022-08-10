4272b27ad2d72c08a239460d43a80edba1de38fc
Colleen Murphy <colleen.murphy@suse.com>
2022-08-10 10:50:29 -0700

    Initial commit

---
+++ rotate-tokens-2.5.sh
@@ -0,0 +1,147 @@
+#!/bin/bash -e
+
+usage() {
+    echo 'For use on Rancher v2.5'
+    echo 'TOKEN=<token> KUBECONFIG=/path/to/local/kubeconfig RANCHER_SERVER=<https://rancher.server> ./rotate-tokens.sh'
+    exit 0
+}
+
+if [ "$1" == "help" ] ; then
+    usage
+fi
+
+if [ "$TOKEN" == "" ]
+then
+    echo 'Create an API token in the Rancher UI and set the environment variable TOKEN before running this script.'
+    exit 1
+fi
+
+if [ "$RANCHER_SERVER" == "" ]
+then
+    echo 'Set $RANCHER_SERVER to point to the Rancher URL.'
+    exit 1
+fi
+
+if curl --insecure -s -u $TOKEN "${RANCHER_SERVER}/v3" | grep Unauthorized >/dev/null
+then
+    echo "Not authorized for Rancher server $RANCHER_SERVER."
+    exit 1
+fi
+
+if ! which kubectl >/dev/null
+then
+    echo 'kubectl and jq must be installed.'
+    exit 1
+fi
+
+if ! which jq >/dev/null
+then
+    echo 'kubectl and jq must be installed.'
+    exit 1
+fi
+
+if ! kubectl get namespace cattle-global-data >/dev/null 2>&1
+then
+    echo 'Set $KUBECONFIG to point to the Rancher local cluster.'
+    exit 1
+fi
+
+cleanup() {
+    kubectl --namespace cattle-system patch deployment cattle-cluster-agent --patch '{"spec": {"template": {"spec": {"serviceAccount": "cattle", "serviceAccountName": "cattle"}}}}'
+    kubectl --namespace cattle-system rollout status deployment cattle-cluster-agent
+    kubectl --namespace cattle-system delete serviceaccount cattle-tmp >/dev/null 2>&1 || true
+    kubectl --namespace cattle-system delete secret cattle-tmp-token >/dev/null 2>&1 || true
+    kubectl delete clusterrolebinding cattle-admin-binding-tmp >/dev/null 2>&1 || true
+}
+
+create_token_secret() {
+    name=$1
+    uid=$2
+    cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: Secret
+metadata:
+  name: $name-token
+  namespace: cattle-system
+  annotations:
+    kubernetes.io/service-account.name: $name
+    kubernetes.io/service-account.uid: $uid
+type: kubernetes.io/service-account-token
+EOF
+}
+
+create_cluster_secret() {
+    cluster=$1
+    uid=$2
+    token=$3
+    secret=$(cat <<EOF | kubectl create --output jsonpath='{.metadata.name}' -f -
+apiVersion: v1
+kind: Secret
+metadata:
+  generateName: cluster-serviceaccounttoken-
+  namespace: cattle-global-data
+  ownerReferences:
+  - apiVersion: management.cattle.io/v3
+    kind: Cluster
+    name: $cluster
+    uid: $uid
+data:
+  credential: $token
+type: opaque
+EOF
+    )
+    echo $secret
+}
+
+clusters=$(kubectl get clusters.management --output jsonpath='{.items[?(.metadata.name != "local")].metadata.name}')
+
+mkdir -p kubeconfigs
+
+MAIN_KUBECONFIG=$KUBECONFIG # may be empty, then default kubeconfig is used
+
+for c in $clusters
+do
+    kubeconfig=$(curl --insecure -s -u $TOKEN \
+        -X POST \
+        -H 'Accept: application/json' \
+        -H 'Content-Type: application/json' \
+        -d '{}' \
+        "$RANCHER_SERVER/v3/clusters/${c}?action=generateKubeconfig" | jq -r .config)
+    echo "$kubeconfig" > kubeconfigs/${c}.config
+    KUBECONFIG=kubeconfigs/${c}.config
+
+    # cleanup in case the script was interrupted before
+    cleanup
+
+    # create temporary admin account
+    tmpuid=$(kubectl --namespace cattle-system create serviceaccount cattle-tmp --output jsonpath='{.metadata.uid}')
+    create_token_secret cattle-tmp $tmpuid
+    kubectl create clusterrolebinding --clusterrole cattle-admin --serviceaccount cattle-system:cattle-tmp cattle-admin-binding-tmp
+    token=$(kubectl --namespace cattle-system get secret cattle-tmp-token --output jsonpath='{.data.token}')
+    kubectl --namespace cattle-system patch deployment cattle-cluster-agent --patch '{"spec": {"template": {"spec": {"serviceAccount": "cattle-tmp", "serviceAccountName": "cattle-tmp"}}}}'
+    kubectl --namespace cattle-system rollout status deployment cattle-cluster-agent
+
+    # set cluster to use temporary account
+    KUBECONFIG=$MAIN_KUBECONFIG
+    old_secret=$(kubectl get clusters.management $c --output jsonpath='{.status.serviceAccountTokenSecret}')
+    cluster_uid=$(kubectl get clusters.management $c --output jsonpath='{.metadata.uid}')
+    secret=$(create_cluster_secret $c $cluster_uid $token)
+    kubectl patch clusters.management $c --patch '{"status": {"serviceAccountTokenSecret": "'$secret'"}}' --type=merge
+    kubectl --namespace cattle-global-data delete secret $old_secret
+
+    # regenerate service account and secret
+    KUBECONFIG=kubeconfigs/${c}.config
+    kubectl --namespace cattle-system delete serviceaccount cattle
+    kubectl --namespace cattle-system create serviceaccount cattle
+    tokensecret=$(kubectl --namespace cattle-system get serviceaccount cattle --output jsonpath='{.secrets[0].name}')
+
+    # restore back to old account
+    token=$(kubectl --namespace cattle-system get secret $tokensecret --output jsonpath='{.data.token}')
+    KUBECONFIG=$MAIN_KUBECONFIG
+    secret=$(create_cluster_secret $c $cluster_uid $token)
+    kubectl patch clusters.management $c --patch '{"status": {"serviceAccountTokenSecret": "'$secret'"}}' --type=merge
+
+    # cleanup temporary artifacts
+    KUBECONFIG=kubeconfigs/${c}.config
+    cleanup
+done
